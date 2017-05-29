35863bfd631ad5919ed81f953609cb697862c66d
Korsunow Konstantin <korsunowk@yandex.ua>
2017-05-29 16:37:48 +0300

    delete key from settings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-INTEGRATION_KEY = 'test_ik_lTp-YAT16nvn74DXKFFoYw'
+INTEGRATION_KEY = '*****************************'
 EBANX_API_PAYMENT_URL = 'https://sandbox.ebanx.com/ws/direct'
 EBANX_API_CANCEL_URL = 'https://sandbox.ebanx.com/ws/cancel'
 EBANX_API_TOKEN_URL = 'https://sandbox.ebanx.com/ws/token'
